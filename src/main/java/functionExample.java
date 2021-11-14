a2f3d1c3978322858e95290f50ff9906cf39444a
Felipe_Choque <felipe.choque@rycconsultores.cl>
2021-11-14 17:18:16 -0300

    v1

---
+++ src/main/java/functionExample.java
@@ -0,0 +1,15 @@
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+
+import java.io.BufferedWriter;
+
+public class functionExample implements HttpFunction {
+
+
+    @Override
+    public void service(HttpRequest httpRequest, HttpResponse httpResponse) throws Exception {
+        BufferedWriter writer = httpResponse.getWriter();
+        writer.write("Hello World!");
+    }
+}
